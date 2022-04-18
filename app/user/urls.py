812783aeec38e38cdd26d7fa4ac37a72f5d29035
Susainathan <susai@digitalprizm.net>
2022-04-19 05:23:37 +0530

    added user aqpi

--- app/user/urls.py
+++ app/user/urls.py
@@ -7,5 +7,6 @@ app_name = 'user'
 urlpatterns = [
     path('create/', views.UserCreateView.as_view(), name='create'),
     path('token/', views.CreateToken.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
 
 ]
